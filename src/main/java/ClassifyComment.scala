02d96aec5120f959fc753f1ab494c97a1b8a6dca
dd <dd@macdeMacBook-Air.local>
2017-11-16 09:51:36 +0800

    fix over memory problem

--- src/main/java/ClassifyComment.scala
+++ src/main/java/ClassifyComment.scala
@@ -4,11 +4,10 @@ import org.apache.spark.ml.classification.NaiveBayes
 import org.apache.spark.ml.feature.{HashingTF, IDF, Tokenizer}
 import org.apache.spark.sql.{DataFrame, Dataset, Row}
 import org.apache.spark.{SparkConf, SparkContext}
-import org.apache.spark.sql.types.{StringType, StructField, StructType}
-import org.apache.spark.mllib.linalg.Vector
-import org.apache.spark.mllib.linalg.Vectors
-import org.apache.spark.mllib.regression.LabeledPoint
-import org.apache.spark.rdd.RDD
+import org.apache.spark.ml.linalg.Vector
+import org.apache.spark.ml.linalg.Vectors
+import org.apache.spark.ml.feature.LabeledPoint
+
 
 import scala.collection.JavaConversions._
 
@@ -19,9 +18,9 @@ object ClassifyComment extends App {
     .setAppName("SparkClassify")
     .setMaster("local[2]")
     .set("spark.driver.host", "localhost")
-    .set("spark.mongodb.input.uri", "mongodb://zc-slave/jd.comment_word")
+    .set("spark.mongodb.input.uri", "mongodb://127.0.0.1/jd.comment_word")
   val sc = new SparkContext(sparkConf);
-  def getCommentVector(readConfig: ReadConfig) = {
+  def getCommentVector(readConfig: ReadConfig) : Dataset[LabeledPoint]= {
 
     val commentRDD = MongoSpark.load(sc, readConfig)
     val sqlContext = new org.apache.spark.sql.SQLContext(sc)
@@ -43,28 +42,30 @@ object ClassifyComment extends App {
     val idfModel = idf.fit(featurizedData)
 
     val rescaledData = idfModel.transform(featurizedData)
-    rescaledData.select("category", "features").show()
+//    rescaledData.select("category", "features").show()
     //转换成Bayes的输入格式
 
     var trainDataRdd = rescaledData.select("category","features").map {
       case Row(label: String, features: Vector) =>
-        (label.toDouble, Vectors.dense(features.toArray))
+        LabeledPoint(label.toDouble, Vectors.dense(features.toArray))
     }
-//    return trainDataRdd
+//    trainDataRdd.take(1).foreach(println)
+    return trainDataRdd
 
   }
 
   def naiveBayes(): Unit ={
     val readTrainData = ReadConfig(
       Map(
-        "uri" -> "mongodb://zc-slave:27017",
+        "uri" -> "mongodb://127.0.0.1:27017",
         "database" -> "jd",
-        "collection" -> "word_test"), Some(ReadConfig(sc)))
+        "collection" -> "all_words"), Some(ReadConfig(sc)))
     val readTestData = ReadConfig(Map(
       "uri" -> "mongodb://zc-slave:27017",
       "database" -> "jd",
       "collection" -> "test_word"), Some(ReadConfig(sc)))
-    getCommentVector(readTrainData)
+    val trainData = getCommentVector(readTrainData)
+    trainData.take(1).foreach(println)
 //    val trainData = getCommentVector(readTrainData)
 //    trainData.show()
 //    val testData = getCommentVector(readTestData)
